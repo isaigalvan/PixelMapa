e063e26373a9fa028813cffa14f821e624b699d3
isai galvan alaniz <isaig2015@gmail.com>
2019-05-02 21:57:55 -0500

    MOVIMIENTO DE LOS PASTELES

---
+++ Assets/Scripts/Pasteles/CrearPasteles.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrearPasteles : MonoBehaviour
+{
+    public GameObject[] pastelPrefab = new GameObject[4];
+    public Transform pastelParent;
+    public float tiempo, tiempo1;
+    public int noPastel, noPastel1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        crearPastelIzq();
+        crearPastelDer();
+        tiempo = Time.deltaTime + tiempo;
+        tiempo1 = Time.deltaTime + tiempo1;
+    }
+
+    public void crearPastelIzq()
+    {
+        if (tiempo >= 1f)
+        {
+            tiempo = 0;
+            if (noPastel == 0)
+            {
+                noPastel = 1;
+                GameObject pastelTemp = Instantiate(pastelPrefab[0], new Vector3(-10, 2.85f, 0), Quaternion.Euler(new Vector3(0, 180, 0)));
+                pastelTemp.transform.parent = pastelParent;
+            }
+            else
+            {
+                GameObject pastelTemp = Instantiate(pastelPrefab[1], new Vector3(-10, 2.85f, 0), Quaternion.Euler(new Vector3(0, 180, 0)));
+                pastelTemp.transform.parent = pastelParent;
+                noPastel = 0;
+            }
+        }
+    }
+
+    public void crearPastelDer()
+    {
+        if (tiempo1 >= 1f)
+        {
+            tiempo1 = 0;
+            if (noPastel1 == 0)
+            {
+                noPastel1 = 1;
+                GameObject pastelTemp1 = Instantiate(pastelPrefab[2], new Vector3(10, 2.85f, 0), Quaternion.Euler(new Vector3(0, 180, 0)));
+                pastelTemp1.transform.parent = pastelParent;
+            }
+            else
+            {
+                GameObject pastelTemp1 = Instantiate(pastelPrefab[3], new Vector3(10, 2.85f, 0), Quaternion.Euler(new Vector3(0, 180, 0)));
+                pastelTemp1.transform.parent = pastelParent;
+                noPastel1 = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Pasteles/PastelDer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PastelDer : MonoBehaviour
+{
+    public float posx = 10, posy = 2.85f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mover();
+    }
+
+
+    public void mover()
+    {
+        if (posx >= 2f)
+        {
+            gameObject.transform.localPosition = new Vector3(posx, posy);
+            posx = posx - 0.05f;
+        }
+        else
+        {
+            if (posy <= 4.3f)
+            {
+                posy = posy + 0.05f;
+                posx = posx - 0.035f;
+                gameObject.transform.localPosition = new Vector3(posx, posy);
+            }
+            else
+            {
+                posy = posy + 0.05f;
+                gameObject.transform.localPosition = new Vector3(posx, posy);
+            }
+        }
+        if (posy >= 7)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
