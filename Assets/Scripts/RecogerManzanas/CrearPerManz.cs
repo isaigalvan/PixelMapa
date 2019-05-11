911a179e5ff14249afec112c4c4889f8eb9eb61c
isai galvan alaniz <isaig2015@gmail.com>
2019-05-10 22:56:30 -0500

    ANIMATOR CONTROLLER MANZANAS, COMIENZO DEL MOVIMIENTO DE PERSONAJES

---
+++ Assets/Scripts/RecogerManzanas/CrearPerManz.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrearPerManz : MonoBehaviour
+{
+    public GameObject perPrefab, per;
+    public Transform perParent;
+    public int idPersonaje;
+    public int posx, posy;
+    public BoxCollider2D box;
+    void Start()
+    {
+        // idPersonaje = RestablecerValores.idPersonaje;
+        Crear();
+    }
+
+    public void Crear()
+    {
+        GameObject personajeTemp = Instantiate(perPrefab, new Vector3(posx, posy, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+        personajeTemp.transform.parent = perParent;
+        per = GameObject.FindGameObjectWithTag("Personaje");
+       // box = per.GetComponent<BoxCollider2D>();
+        // AsignarNombres();
+        //AsignarTamanos();
+        asignarTexturas();
+    }
+
+    public void asignarTexturas()
+    {
+        switch (idPersonaje)
+        {
+            case 0:
+                per.GetComponent<Animator>().SetFloat("personaje", 0f);
+                break;
+            case 1:
+                per.GetComponent<Animator>().SetFloat("personaje", 1f);
+                break;
+            case 2:
+                per.GetComponent<Animator>().SetFloat("personaje", 2f);
+                break;
+            case 3:
+                per.GetComponent<Animator>().SetFloat("personaje", 3f);
+                per.transform.localScale = new Vector3(0.3f, 0.3f);
+                box.size = new Vector3(5.6f, 8);
+                box.offset = new Vector3(0.1f, -3.519004f);
+                break;
+            case 4:
+                per.GetComponent<Animator>().SetFloat("personaje", 4f);
+                break;
+            case 5:
+                per.GetComponent<Animator>().SetFloat("personaje", 5f);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/RecogerManzanas/PerManz.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerManz : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mover();
+        sr= gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void mover()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.GetComponent<Animator>().SetBool("caminando", true);
+            sr.flipX = true;
+            this.transform.position += new Vector3(-0.07f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.GetComponent<Animator>().SetBool("caminando", true);
+            sr.flipX = false;
+            this.transform.position += new Vector3(0.07f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == false && Input.GetKey(KeyCode.RightArrow) == false && Input.GetKey(KeyCode.UpArrow) == false && Input.GetKey(KeyCode.DownArrow) == false)
+        {
+            gameObject.GetComponent<Animator>().SetBool("caminando", false);
+        }
+    }
+}
