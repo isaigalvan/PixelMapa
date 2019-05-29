abe458c4d95334dd1e09bb1eb7f99e5ab885baa1
eldesdoll <38787939+eldesdoll@users.noreply.github.com>
2019-05-29 10:11:18 -0500

    Infla el globo 1/2
    Falta acomodar las animaciones

---
+++ Assets/Scripts/Minijuegos/ReventarGlobos/clicker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clicker : MonoBehaviour
+{
+    public bool ganador = false;
+    public bool perdedor = false;
+    public Button boton;
+    public int maxClicks = 20;
+
+    private int clicks = 0;
+
+    public void BotonClick()
+    {
+        clicks++;
+
+        if (clicks == maxClicks)
+        {
+            ganador = true;
+            gameObject.GetComponent<Animator>().SetBool("perdedor", perdedor);
+            gameObject.GetComponent<Animator>().SetBool("ganador", ganador);
+            Debug.Log("__________Ganador__________");
+        }
+        else if(clicks > maxClicks)
+        {
+            boton.interactable = false;
+            perdedor = true;
+            gameObject.GetComponent<Animator>().SetBool("perdedor", perdedor);
+            gameObject.GetComponent<Animator>().SetBool("ganador", ganador);
+            Debug.Log("__________Perdedor__________");
+
+        }
+
+        Debug.Log(clicks);
+    }
+}
