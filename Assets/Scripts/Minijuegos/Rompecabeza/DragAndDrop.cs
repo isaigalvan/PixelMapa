63c608eaea4e0b1693453110604534b6db7883a9
eldesdoll <38787939+eldesdoll@users.noreply.github.com>
2019-05-28 20:36:44 -0500

    rompecabezas
    rompecabezas 3x3 terminado

---
+++ Assets/Scripts/Minijuegos/Rompecabeza/DragAndDrop.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour {
+
+	private Vector3 screenSpace;
+    private Vector3 offset;
+    private Sensores sensores;
+    private Vector3 posInicial;
+    private Puzzle puzzle;
+	private bool moviendoLeft, moviendoRight, moviendoUp, moviendoDown;
+    private int numMovimientos = 0;
+
+	void Awake(){
+		sensores = GetComponentInChildren(typeof(Sensores)) as Sensores;
+		puzzle = GameObject.Find ("Scripts").GetComponent (typeof(Puzzle)) as Puzzle;
+	}
+
+	void OnMouseDown(){
+		screenSpace = Camera.main.WorldToScreenPoint(transform.position);
+		offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,0));
+		posInicial = transform.position;
+	}
+
+	void OnMouseDrag () {
+		Vector3 posicion=new Vector3 (Input.mousePosition.x, Input.mousePosition.y , 0); 
+		Vector3 curScreenSpace = posicion;  
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;	
+			
+		if (!sensores.ocupadoLeft  || !sensores.ocupadoRight) {				
+			curPosition = new Vector3 (curPosition.x, transform.position.y, 0);
+			if (!sensores.ocupadoLeft && !moviendoLeft && !moviendoRight) {
+				moviendoLeft = true;
+			}
+			if (!sensores.ocupadoRight && !moviendoRight && !moviendoLeft) {
+				moviendoRight = true;
+			}
+		} else if (!sensores.ocupadoUp  || !sensores.ocupadoDown) {					
+			curPosition = new Vector3 (transform.position.x, curPosition.y, 0);
+			if (!sensores.ocupadoUp && !moviendoUp && !moviendoDown) {
+				moviendoUp = true;
+			}
+			if (!sensores.ocupadoDown && !moviendoDown && !moviendoUp) {
+				moviendoDown = true;
+			}
+		} else {
+			return;																	
+		}
+
+
+		if (moviendoLeft) {
+			if (curPosition.x > posInicial.x) {
+				return;
+			}
+		}
+		if (moviendoRight) {
+			if (curPosition.x < posInicial.x) {
+				return ;
+			}
+		}
+		if (moviendoUp) {
+			if (curPosition.y < posInicial.y) {
+				return;
+			}
+		}
+		if (moviendoDown) {
+			if (curPosition.y > posInicial.y) {
+				return ;
+			}
+		}
+
+		if(Vector3.Distance(curPosition,posInicial)>1){return;}						
+		transform.position = curPosition;												
+	}
+
+
+
+	void OnMouseUp(){
+		transform.position=new Vector3(Mathf.Round(transform.position.x),Mathf.Round(transform.position.y),0);
+		moviendoLeft = false;
+		moviendoRight = false;
+		moviendoUp = false;
+		moviendoDown = false;
+		puzzle.ComprobarGanador();
+
+        numMovimientos ++;
+        Debug.Log(numMovimientos);
+    }
+}
---
+++ Assets/Scripts/Minijuegos/Rompecabeza/Puzzle.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Puzzle : MonoBehaviour {
+
+	public List<Sprite> fichaImg=new List<Sprite>();
+	public GameObject fichaPrfb;
+	public GameObject bordePrfb;
+	public Sprite fichaEscondidaImg;
+	public GameObject textoGanador;
+
+    private GameObject fichaEscondida;
+    private int numCostado;
+    private GameObject padreFichas;
+    private GameObject padreBordes;
+    private List<Vector3> posicionesIniciales = new List<Vector3>();
+    private GameObject[] _fichas;
+
+	void Awake()
+    {
+		padreFichas=GameObject.Find("Fichas");
+		padreBordes = GameObject.Find ("Bordes");
+	}
+
+	void Start ()
+    {
+		if (Mathf.Sqrt (fichaImg.Count)  == Mathf.Round (Mathf.Sqrt (fichaImg.Count)))
+        {
+			CrearFichas ();
+		}
+	}
+	
+	void CrearFichas(){
+		int contador=0;
+		numCostado = (int) Mathf.Sqrt (fichaImg.Count);
+
+		for (int alto = numCostado+2; alto >0; alto--)
+        {
+			for (int ancho = 0; ancho < numCostado+2; ancho++)
+            {
+				Vector3 posicion = new Vector3 (ancho - (numCostado / 2), alto - (numCostado / 2),0);
+
+				if (alto == 1 || alto == numCostado +2 || ancho == 0 || ancho == numCostado+1)
+                { 	
+					GameObject borde = Instantiate (bordePrfb, posicion, Quaternion.identity);		 
+					borde.transform.parent = padreBordes.transform;									
+				}
+                else
+                {																			
+					GameObject ficha = Instantiate (fichaPrfb, posicion, Quaternion.identity);		
+					ficha.GetComponent<SpriteRenderer> ().sprite = fichaImg [contador];				
+					ficha.transform.parent = padreFichas.transform;
+					ficha.name=fichaImg [contador].name;											
+					if (ficha.name == fichaEscondidaImg.name)
+                    {										
+						fichaEscondida  = ficha;													
+					}
+					contador++;
+				}
+			}
+		}
+			
+		fichaEscondida.gameObject.SetActive(false);
+		_fichas=(GameObject.FindGameObjectsWithTag("Ficha"));
+		for (int i = 0; i < _fichas.Length; i++) {
+			posicionesIniciales.Add(_fichas [i].transform.position);
+		}
+		Barajar ();
+	}
+
+	void Barajar()
+    {
+		int aleatorio;
+
+		for (int i = 0; i < _fichas.Length; i++)
+        {
+			aleatorio = Random.Range (i, _fichas.Length);								
+			Vector3 posTemp = _fichas [i].transform.position;							
+			_fichas [i].transform.position = _fichas [aleatorio].transform.position  ;	 
+			_fichas [aleatorio].transform.position  = posTemp;																										
+		}
+	}
+
+    public void ComprobarGanador()
+    {
+        for (int i = 0; i < _fichas.Length; i++)
+        {
+            if (posicionesIniciales[i] != _fichas[i].transform.position)
+            {
+                return;                             
+            }
+        }
+
+        fichaEscondida.gameObject.SetActive(true); 
+        textoGanador.gameObject.SetActive(true);
+    }
+}
